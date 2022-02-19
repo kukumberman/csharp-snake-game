20247bf851e85e96d5d30d10fd2af802a622dcf7
Dima <strongbaby1337@gmail.com>
2022-02-19 16:53:40 +0200

    vec2 struct

--- singleplayer/Program.cs
+++ singleplayer/Program.cs
@@ -5,27 +5,6 @@ using System.Runtime.InteropServices;
 
 namespace singleplayer
 {
-    struct Vector2Int
-    {
-        public int X, Y;
-
-        public Vector2Int(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public static bool operator ==(Vector2Int a, Vector2Int b)
-        {
-            return a.X == b.X && a.Y == b.Y;
-        }
-
-        public static bool operator !=(Vector2Int a, Vector2Int b)
-        {
-            return !(a == b);
-        }
-    }
-
     class Program
     {
         [DllImport("User32.dll")]
---
+++ singleplayer/Vector2Int.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleplayer
+{
+    struct Vector2Int
+    {
+        public int X, Y;
+
+        public Vector2Int(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static bool operator ==(Vector2Int a, Vector2Int b)
+        {
+            return a.X == b.X && a.Y == b.Y;
+        }
+
+        public static bool operator !=(Vector2Int a, Vector2Int b)
+        {
+            return !(a == b);
+        }
+    }
+}
